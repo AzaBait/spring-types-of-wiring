7069aebee53bd0bdf72b2234e9c7e85d81814268
dell <m>
2021-10-15 17:33:29 +0600

    Spring-types-of-wiring

--- src/main/java/peaksoft/config/AppConfig.java
+++ src/main/java/peaksoft/config/AppConfig.java
@@ -1,10 +1,10 @@
 package peaksoft.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import peaksoft.model.Island2;
-import peaksoft.model.Wood3;
+import peaksoft.model.*;
 
 @Configuration
 @ComponentScan(basePackages = "peaksoft")
@@ -14,4 +14,24 @@ public class AppConfig {
     public static Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
     }
+
+//    @Bean
+//    public Wood3 getWood3(Rabbit4 rabbit4) {
+//        return new Wood3(rabbit4);
+//    }
+//
+//    @Bean
+//    public Rabbit4 getRabbit4(Duck5 duck5) {
+//        return new Rabbit4(duck5);
+//    }
+//
+//    @Bean
+//    public Duck5 getDuck5 (Egg6 egg6) {
+//        return new Duck5(egg6);
+//    }
+//
+//    @Bean
+//    public Egg6 get () {
+//        return ;
+//    }
 }
